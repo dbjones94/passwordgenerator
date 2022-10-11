52a991a62f7479bde753a350348f6923ad0ca9e6
dbjones94 <114501993+dbjones94@users.noreply.github.com>
2022-10-11 09:42:38 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Password_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static bool addUpperCase;
+        private static bool addNumbers;
+        private static bool addSpecials;
+        private static string Chars;
+        private static int length;
+
+
+
+        private void uppercaseCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            addUpperCase = true;
+        }
+        private void uppercaseCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            addUpperCase = false;
+        }
+
+        private void numbersCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            addNumbers = true;
+        }
+
+        private void numbersCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            addNumbers = false;
+        }
+
+        private void specialCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            addSpecials = true;
+        }
+        private void specialCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            addSpecials = false;
+        }
+
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            length = (int)lengthSlider.Value;
+            if (addUpperCase && addNumbers && addSpecials)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!@#$%^&*";
+            }
+            else if (addUpperCase && addNumbers)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            }
+            else if (addUpperCase && addSpecials)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!@#$%^&*";
+            }
+            else if (addNumbers && addSpecials)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyz1234567890?!@#$%^&*";
+            }
+            else if (addUpperCase)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+            else if (addNumbers)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+            }
+            else if (addSpecials)
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyz?!@#$%^&*";
+            }
+            else
+            {
+                Chars = "abcdefghijklmnopqrstuvwxyz";
+            }
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+            while (0 < length--)
+            {
+                sb.Append(Chars[random.Next(Chars.Length)]);
+            }
+            string password = sb.ToString();
+            pwTextBox.Text = password;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(pwTextBox.Text);
+        }
+
+        private void lengthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            length = (int)lengthSlider.Value;
+        }
+
+    }
+}
